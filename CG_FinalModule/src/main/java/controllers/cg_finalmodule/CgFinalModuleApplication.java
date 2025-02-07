dadf65c6b1e91a39de1799bd01d638d22b2a4924
TranDung147 <dungttde180631@fpt.edu.vn>
2025-02-07 19:51:26 +0700

    layout

---
+++ CG_FinalModule/src/main/java/controllers/cg_finalmodule/CgFinalModuleApplication.java
@@ -0,0 +1,13 @@
+package controllers.cg_finalmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgFinalModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgFinalModuleApplication.class, args);
+    }
+
+}
---
+++ CG_FinalModule/src/test/java/controllers/cg_finalmodule/CgFinalModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package controllers.cg_finalmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgFinalModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
