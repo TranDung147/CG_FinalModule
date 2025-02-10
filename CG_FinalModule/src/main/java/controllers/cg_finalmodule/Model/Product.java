7e391b4900034e743624117fe9dd5d858941865a
TranDung147 <dungttde180631@fpt.edu.vn>
2025-02-10 19:39:52 +0700

    done

---
+++ CG_FinalModule/src/main/java/controllers/cg_finalmodule/Model/Product.java
@@ -0,0 +1,16 @@
+package controllers.cg_finalmodule.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+    private String productName;
+    private Double price;
+    
+}
