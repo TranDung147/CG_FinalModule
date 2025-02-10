74b8542a817641398e7eff6499294f1da19cf3a4
NguyenBaoAnhThu <125013548+NguyenBaoAnhThu@users.noreply.github.com>
2025-02-10 20:01:38 +0700

    done
    done

---
+++ CG_FinalModule/src/main/java/controllers/cg_finalmodule/Model/Role.java
@@ -0,0 +1,21 @@
+package controllers.cg_finalmodule.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleId;
+    private String roleName;
+}
+
