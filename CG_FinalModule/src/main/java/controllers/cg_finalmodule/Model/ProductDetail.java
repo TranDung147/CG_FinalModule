091a77f7e9ce20d834e0a6911740d73069720d0c
TranDung147 <dungttde180631@fpt.edu.vn>
2025-02-10 21:12:50 +0700

    Done

---
+++ CG_FinalModule/src/main/java/controllers/cg_finalmodule/Model/ProductDetail.java
@@ -0,0 +1,40 @@
+package controllers.cg_finalmodule.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product_detail")
+public class ProductDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productDetailId;
+
+    @NotNull(message = "Kích thước không được để trống")
+    @Digits(integer = 2, fraction = 1, message = "Kích thước phải có tối đa 2 chữ số nguyên và 1 chữ số thập phân")
+    private Double size;
+
+    @Size(max = 20, message = "Màu sắc tối đa 20 ký tự")
+    private String color;
+
+    @Size(max = 50, message = "Phong cách tối đa 50 ký tự")
+    private String style;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Giá bán lẻ phải lớn hơn 0")
+    @Digits(integer = 10, fraction = 2, message = "Giá bán lẻ không hợp lệ")
+    private BigDecimal retailPrice;
+
+    @OneToOne
+    @JoinColumn(name = "productId", referencedColumnName = "productId", nullable = false, unique = true)
+    private Product product;
+}
