d6c46af8c8448ae417b4700f39325fd4e669e9ef
TranDung147 <dungttde180631@fpt.edu.vn>
2025-02-14 19:53:17 +0700

    done delete employee

--- finalModule/finalModule/src/main/java/com/codegym/finalModule/controller/AdminHomeController.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/controller/AdminHomeController.java
@@ -1,14 +1,39 @@
 package com.codegym.finalModule.controller;
 
+import com.codegym.finalModule.service.Interface.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class AdminHomeController {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
     @GetMapping("/Admin")
     public String showAdminHome() {
         return "admin/layout/layout";
+    }
+
+    @PostMapping("/delete")
+    public String deleteEmployees(@RequestParam(value = "employeeIds", required = false) List<Integer> employeeIds,
+                                  RedirectAttributes redirectAttributes) {
+        if (employeeIds != null && !employeeIds.isEmpty()) {
+            List<String> deletedEmployees = employeeService.getEmployeeNamesByIds(employeeIds);
+            employeeService.deleteEmployeeByID(employeeIds);
+
+            String message = "Xóa nhân viên: " + String.join(", ", deletedEmployees) + " thành công!";
+            redirectAttributes.addFlashAttribute("message", message);
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Không có nhân viên nào được chọn để xóa!");
+        }
+        return "redirect:/employees";
     }
 }
 
--- finalModule/finalModule/src/main/java/com/codegym/finalModule/service/Class/EmployeeService.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/service/Class/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.codegym.finalModule.service.Class;
 
+import com.codegym.finalModule.model.Employee;
 import com.codegym.finalModule.repository.IEmployeeRepository;
 import com.codegym.finalModule.service.Interface.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class EmployeeService implements IEmployeeService {
@@ -15,10 +18,13 @@ public class EmployeeService implements IEmployeeService {
     @Autowired
     private IEmployeeRepository employeeRepository;
 
-    public void deleteEmployeeByID(Integer employeeId) {
-        if (!employeeRepository.existsById(employeeId)) {
-            throw new RuntimeException("ID này không tồn tại!");
-        }
-        employeeRepository.deleteById(employeeId);
+    @Override
+    public void deleteEmployeeByID(List<Integer> employeeIds) {
+        employeeRepository.deleteAllById(employeeIds);
+    }
+
+    @Override
+    public List<String> getEmployeeNamesByIds(List<Integer> employeeIds) {
+        return employeeRepository.findAllById(employeeIds).stream().map(Employee::getEmployeeName).toList();
     }
 }
