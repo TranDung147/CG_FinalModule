c545849658d8ffdc3f4f20f864690c13b708b631
Baotuan2k4bt <tuannguyen2k4bt@gmail.com>
2025-02-14 23:14:44 +0700

    List va tim kiem danh sach nhan vien

--- finalModule/finalModule/src/main/java/com/codegym/finalModule/controller/AdminHomeController.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/controller/AdminHomeController.java
@@ -1,14 +1,58 @@
 package com.codegym.finalModule.controller;
 
+import com.codegym.finalModule.model.Employee;
+import com.codegym.finalModule.service.Interface.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/Admin")
 public class AdminHomeController {
+    @Autowired
+    private IEmployeeService iemployeeService;
+    //    @GetMapping("/Admin")
+//    public String showAdminHome() {
+//
+//        return "admin/layout/layout";
+//    }
+    @GetMapping
+    public String EmployeeList(
+            Model model,
+            @RequestParam(name = "keyword", required = false, defaultValue = "") String keyword,
+            @RequestParam(name = "type", required = false, defaultValue = "all") String type,
+            @RequestParam(name = "page", required = false, defaultValue = "1") Integer pageNo){
+        Page<Employee> list = iemployeeService.searchUsers(keyword, type, pageNo);
 
-    @GetMapping("/Admin")
-    public String showAdminHome() {
-        return "admin/layout/layout";
+        // Đưa dữ liệu vào model
+        model.addAttribute("list", list.getContent());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", list.getTotalPages());
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("type", type);
+
+        return "admin/layout/layout"; // Trả về trang giao diện
     }
+
+    //    @PostMapping("delete/{employeeId}")
+//    public String deleteEmployee(@PathVariable("employeeId") Integer employeeId, RedirectAttributes redirectAttributes) {
+//        try {
+//            iemployeeService.deleteEmployeeByID(employeeId);
+//            redirectAttributes.addFlashAttribute("message", "Xóa thành công khuyến mãi!");
+//        } catch (RuntimeException e) {
+//            redirectAttributes.addFlashAttribute("error", "Xóa thất bại. Có lỗi xảy ra: " + e.getMessage());
+//        }
+//        return "redirect:/Admin";
+//    }
+    @GetMapping("/Admin/delete")
+    public String delete(@RequestParam("employeeId") Integer employeeId) {
+        iemployeeService.deleteEmployeeByID(employeeId);
+        return "redirect:/Admin";
+    }
+
+
 }
 
--- finalModule/finalModule/src/main/java/com/codegym/finalModule/service/Class/EmployeeService.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/service/Class/EmployeeService.java
@@ -1,12 +1,17 @@
 package com.codegym.finalModule.service.Class;
 
+import com.codegym.finalModule.model.Employee;
 import com.codegym.finalModule.repository.IEmployeeRepository;
 import com.codegym.finalModule.service.Interface.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,10 +20,38 @@ public class EmployeeService implements IEmployeeService {
     @Autowired
     private IEmployeeRepository employeeRepository;
 
-    public void deleteEmployeeByID(Integer employeeId) {
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public List<Employee> findByKeyword(String keyword) {
+        return employeeRepository.searchByKeywordAndType(keyword, "all", PageRequest.of(0, Integer.MAX_VALUE)).getContent();
+    }
+
+    @Override
+    public Page<Employee> findAll(Integer pageNo) {
+        Pageable pageable = PageRequest.of(pageNo - 1, 5);
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Employee> findByKeyword(String keyword, Integer pageNo) {
+        Pageable pageable = PageRequest.of(pageNo - 1, 5);
+        return employeeRepository.searchByKeywordAndType(keyword, "all", pageable);
+    }
+
+    @Override
+    public Page<Employee> searchUsers(String keyword, String type, Integer pageNo) {
+        Pageable pageable = PageRequest.of(pageNo - 1, 5);
+        return employeeRepository.searchByKeywordAndType(keyword, type, pageable);
+
+    }
+        public void deleteEmployeeByID(Integer employeeId) {
         if (!employeeRepository.existsById(employeeId)) {
             throw new RuntimeException("ID này không tồn tại!");
         }
         employeeRepository.deleteById(employeeId);
-    }
-}
+
+}}
