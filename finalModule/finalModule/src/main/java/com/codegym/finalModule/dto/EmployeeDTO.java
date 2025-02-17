656069006720c0bde1ebe089cb503b820b3baf6d
tranthekyky <ttk8701ad@gmail.com>
2025-02-17 10:49:04 +0700

    Employee Task

--- finalModule/finalModule/src/main/java/com/codegym/finalModule/dto/EmployeeDTO.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/dto/EmployeeDTO.java
@@ -1,4 +1,25 @@
 package com.codegym.finalModule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EmployeeDTO {
+
+    @NotBlank(message = "Tên không được để trống !")
+    private String employeeName;
+    private LocalDate employeeBirthday;
+    @NotBlank(message = "Địa chỉ không được để trống !")
+    private String employeeAddress;
+
+    private String employeePhone;
+    private String employeeEmail;
+    private String employeeWork;
 }
