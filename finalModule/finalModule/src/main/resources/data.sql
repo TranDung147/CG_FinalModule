cf8f17475f32cd95e4e604dbe506e29c2dc676d2
NguyenBaoAnhThu <125013548+NguyenBaoAnhThu@users.noreply.github.com>
2025-02-20 22:40:23 +0700

    Update data.sql

--- finalModule/finalModule/src/main/resources/data.sql
+++ finalModule/finalModule/src/main/resources/data.sql
@@ -1,23 +1,23 @@
-#Thêm dữ liệu vào category
-INSERT INTO categories (categoryid, name, description) VALUES
-                                                           (1, 'Điện thoại', 'Các dòng điện thoại thông minh mới nhất'),
-                                                           (2, 'Máy tính bảng', 'Máy tính bảng phục vụ công việc và giải trí'),
-                                                           (3, 'Laptop', 'Laptop dành cho học tập, làm việc, gaming'),
-                                                           (4, 'Tai nghe', 'Tai nghe chất lượng cao, có dây và không dây'),
-                                                           (5, 'Tay cầm', 'Tay cầm chơi game các loại'),
-                                                           (6, 'Hàng cũ', 'Sản phẩm đã qua sử dụng, chất lượng tốt'),
-                                                           (7, 'Khuyến mãi', 'Các sản phẩm đang được giảm giá');
+INSERT INTO categories (name, description, create_at, update_at) VALUES
+('Điện thoại', 'Các dòng điện thoại thông minh mới nhất', NOW(), NOW()),
+('Máy tính bảng', 'Máy tính bảng phục vụ công việc và giải trí', NOW(), NOW()),
+('Laptop', 'Laptop dành cho học tập, làm việc, gaming', NOW(), NOW()),
+('Tai nghe', 'Tai nghe chất lượng cao, có dây và không dây', NOW(), NOW()),
+('Tay cầm', 'Tay cầm chơi game các loại', NOW(), NOW()),
+('Hàng cũ', 'Sản phẩm đã qua sử dụng, chất lượng tốt', NOW(), NOW()),
+('Khuyến mãi', 'Các sản phẩm đang được giảm giá', NOW(), NOW());
 
-# Thêm dữ liệu vào brand
+
+
 INSERT INTO brands (name, description, status, country, create_at, update_at)
 VALUES
-    ('Apple', 'Thương hiệu công nghệ hàng đầu', 1, 'USA', NOW(), NOW()),
-    ('Samsung', 'Tập đoàn điện tử Hàn Quốc', 1, 'South Korea', NOW(), NOW()),
-    ('Sony', 'Công ty điện tử Nhật Bản', 1,  'Japan', NOW(), NOW()),
-    ('Asus', 'Hãng sản xuất máy tính và phần cứng nổi tiếng', 1,  'Taiwan', NOW(), NOW()),
-    ('Dell', 'Công ty máy tính hàng đầu thế giới', 1, 'USA', NOW(), NOW());
+('Apple', 'Thương hiệu công nghệ hàng đầu', 1, 'USA', NOW(), NOW()),
+('Samsung', 'Tập đoàn điện tử Hàn Quốc', 1, 'South Korea', NOW(), NOW()),
+('Sony', 'Công ty điện tử Nhật Bản', 1,  'Japan', NOW(), NOW()),
+('Asus', 'Hãng sản xuất máy tính và phần cứng nổi tiếng', 1,  'Taiwan', NOW(), NOW()),
+('Dell', 'Công ty máy tính hàng đầu thế giới', 1, 'USA', NOW(), NOW());
 
-# Thêm dữ liệu vào product
+
 
 INSERT INTO products (name, price, description, create_at, update_at, category_id, brand_id) VALUES
 ('iPhone 15', 21000000, 'Điện thoại Apple iPhone 15 128GB', NOW(), NOW(), 1, 1),
