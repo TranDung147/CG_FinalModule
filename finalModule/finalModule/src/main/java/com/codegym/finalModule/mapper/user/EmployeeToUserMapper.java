906825e5f780aa07a20094946333bcf0a70c2217
Duyvd04 <vuducduy1112004@gmail.com>
2025-03-10 19:57:36 +0700

    Fix encrypted password

--- finalModule/finalModule/src/main/java/com/codegym/finalModule/mapper/user/EmployeeToUserMapper.java
+++ finalModule/finalModule/src/main/java/com/codegym/finalModule/mapper/user/EmployeeToUserMapper.java
@@ -5,26 +5,34 @@ import com.codegym.finalModule.enums.RoleEnums;
 import com.codegym.finalModule.model.Role;
 import com.codegym.finalModule.model.User;
 import com.codegym.finalModule.repository.IRoleRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Component
 public class EmployeeToUserMapper {
     private final IRoleRepository roleRepository;
-    public EmployeeToUserMapper(IRoleRepository roleRepository) {
+    private final PasswordEncoder passwordEncoder; // Inject PasswordEncoder
+
+    public EmployeeToUserMapper(IRoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
-    public User convertEmployeeToUser (EmployeeDTO employeeDTO) {
+
+    public User convertEmployeeToUser(EmployeeDTO employeeDTO) {
         Role role = this.roleRepository.findByRoleName(RoleEnums.ROLE_EMPLOYEE);
-        List<Role> roleList = new ArrayList<>() ;
+        List<Role> roleList = new ArrayList<>();
         roleList.add(role);
+
+        // Hash the password before setting it
+        String hashedPassword = passwordEncoder.encode(employeeDTO.getPassword());
+
         return User.builder()
                 .email(employeeDTO.getEmail())
                 .username(employeeDTO.getUsername())
-                .encrytedPassword(employeeDTO.getPassword())
+                .encrytedPassword(hashedPassword) // Set hashed password
                 .enabled(true)
                 .roles(roleList)
                 .build();
